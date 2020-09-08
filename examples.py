d0a992be7d07b3317f640710121f609d38d1a3c3
Georg Stricker <georg.stricker@gmx.net>
2020-09-08 19:41:26 +0200

    finished PNGCoing, need code restructure for proper usage

--- examples.py
+++ examples.py
@@ -1,8 +1,20 @@
-# Example of PNG Coin validation
-validate_PNG("./images/alice.png")
+from project import PNGcoin as pg
+from PIL import Image
+import pickle
 
 # Create PNGCoin
-my_coin = PNGCoin([
+my_coin = pg.PNGCoin([
     Image.open("./images/alice.png"),
     Image.open("./images/alice-to-bob.png")])
+
+# Check that it works
 my_coin.transfers[1].show()
+
+# Test coin validation
+pg.validate(my_coin)
+
+# Check serialization
+filename = "./bob.pngcoin"
+pg.to_disk(my_coin, filename)
+my_coin_2 = pg.from_disk(filename)
+my_coin.transfers == my_coin_2.transfers
--- project/PNGcoin.py
+++ project/PNGcoin.py
@@ -15,6 +15,38 @@ Contains the following functions:
 """
 
 from PIL import Image
+import pickle
+
+#####################
+# Hellper functions #
+#####################
+
+def handle_user_input(user_input):
+    """Handles user input and returns a bool for valid answers, calls itself
+    otherwise
+
+    Parameters
+    ----------
+    user_input: str
+        A string of user input
+
+    Returns
+    -------
+    bool
+        True or False depending on the input of the user. y = yes, n = no
+    """
+
+    if user_input.lower() == "y":
+        return True
+    elif user_input.lower() == "n":
+        return False
+    else:
+        user_input = input("Please answer only y or n")
+        return handle_user_input(user_input)
+
+###########
+# PNGCoin #
+###########
 
 class PNGCoin:
     """The class of the simple PNGCoin
@@ -33,42 +65,60 @@ class PNGCoin:
     def __init__(self, transfers):
         self.transfers = transfers
 
-def validate():
+    def validate(self):
     """Asks the user for input in a yes/no question
 
     Parameters
     ----------
-    None
+    coin: PNGCoin
+        A coin of type PNGCoin
 
     Returns
     -------
-    bool
-       True or False depending on the input of the user. y = yes, n = no
+    bool 
+        True or False based on the validity of the transfers
     """
     
+    for transfer in self.transfers:
+        transfer.show()
     user_input = input("Is this a valid signature? (y/n)")
-    if user_input.lower() == "y":
-        return True
-    elif user_input.lower() == "n":
-        return False
-    else:
-        validate()
+        valid = handle_user_input(user_input)
+        if not valid:
+            return False
+    return True
 
-def validate_PNG(filename):
-    """Shows a PNG image and asks the user to validate it
+def to_disk(coin, filename):
+    """Writes a Python object to filename in bytecode
+    
+    Parameters
+    ----------
+    coin: PNGCoin
+        A PNGCoin
+    filename: str
+        A file to write the object data to
+
+    Returns
+    -------
+    None
+    """
+    serialized = pickle.dumps(coin)
+    with open(filename, "wb") as f:
+        f.write(serialized)
+
+def from_disk(filename):
+    """Reads a Python object from filename (in bytecode)
 
     Parameters
     ----------
     filename: str
-       A valid filename incl. the path of a PNG image
+        The file to read from
 
     Returns
-    -------
-    void
+    ----------
+    A Python object that was stored in bytecode
     """
+    with open(filename, "rb") as f:
+        serialized = f.read()
+        return pickle.loads(serialized)
     
-    img = Image.open(filename)
-    img.show()
-    validate()
-
-
+    
